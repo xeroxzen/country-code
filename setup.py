9999ac5fe85d475afdc292b299916179fe1e75a0
Andile Jaden aka Google Jr <andilembele020@gmail.com>
2020-04-15 01:54:15 +0200

    Changed version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name= "pycountrycode",
-    version="1.1.5",
+    version="1.1.6",
     description="A neat country call code retriever",
     long_description=README,
     long_description_content_type="text/markdown",
