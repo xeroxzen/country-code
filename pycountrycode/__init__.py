c33a541f50981d871344287bcc73c4281902aa8e
Andile Jaden Mbele <andilembele020@gmail.com>
2023-12-08 18:31:07 +0200

    Update __init__.py

--- pycountrycode/__init__.py
+++ pycountrycode/__init__.py
@@ -2,5 +2,5 @@
 
 __author__ = 'Andile Jaden Mbele'
 __email__ = 'andilembele020@gmail.com'
-__github__ = 'www.github.com/xeroxzen/pycountrycode'
+__github__ = 'www.github.com/xeroxzen/country-code'
 __package__ = 'pycountrycode'
