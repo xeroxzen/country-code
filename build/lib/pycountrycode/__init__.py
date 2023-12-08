171526294a06f537b1531ce91ac4f8608e3414e7
Andile Jaden Mbele <andilembele020@gmail.com>
2023-12-08 18:26:30 +0200

    Delete build directory

--- build/lib/pycountrycode/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = 'Andile Jaden Mbele'
-__email__ = 'andilembele020@gmail.com'
-__github__ = 'www.github.com/xeroxzen/pycountrycode'
-__package__ = 'pycountrycode'
